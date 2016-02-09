850ef7aeec02fedbbfdaba4eb2238e19fdec33ad
Leon Wetzel <leonwetzel@gmail.com>
2016-02-09 20:56:31 +0100

    Added SumOfArray project files

---
+++ Leertaak 1/leertaak1SumofArray/src/ArraySizeException.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Leon Wetzel
+ * Date of creation 9-2-2016, 13:21
+ * |
+ * Authors: Leon Wetzel
+ * |
+ * Version: 1.0
+ * Package: PACKAGE_NAME
+ * Class:
+ * Description:
+ * |
+ * |
+ * Changelog:
+ * 1.0:
+ */
+public class ArraySizeException extends Exception {
+    public ArraySizeException(String message) {
+        super(message);
+    }
+}
---
+++ Leertaak 1/leertaak1SumofArray/src/Main.java
@@ -0,0 +1,51 @@
+/**
+ * Created by Leon Wetzel
+ * Date of creation 9-2-2016, 13:21
+ * |
+ * Authors: Leon Wetzel
+ * |
+ * Version: 1.0
+ * Package: PACKAGE_NAME
+ * Class:
+ * Description:
+ * |
+ * |
+ * Changelog:
+ * 1.0:
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] results = getArraySum(new int[] {3, 6, 9}, new int[] {2, 6, 5});
+            for (int item : results) {
+                System.out.println(item);
+            }
+        } catch(ArraySizeException exception) {
+            System.err.println(exception.getMessage());
+        } finally {
+            System.out.println("Nog een fijne dag!");
+        }
+    }
+
+    /**
+     * Get an array that contains the sum of values of two arrays.
+     * @param a: array containing an amount of integers
+     * @param b: array containing an amount of integers
+     * @return An array that contains the sum of values of a and b.
+     */
+    public static int[] getArraySum(int[] a, int[] b) throws ArraySizeException {
+        if(a.length != b.length) {
+            // arrays bevatten niet evenveel waarden
+            throw new ArraySizeException("Hmm, de lengtes van je arrays komen niet overeen!" +
+                    " Je eerste array bevat " + a.length + " items en je tweede array bevat " +
+                    b.length + " items.");
+        } else {
+            // maak een nieuwe array die de resultaten bevat...
+            int[] result = new int[a.length];
+            for(int i = 0; i < a.length; i++) {
+                result[i] = a[i] + b[i];
+            }
+            return result;
+        }
+    }
+}
