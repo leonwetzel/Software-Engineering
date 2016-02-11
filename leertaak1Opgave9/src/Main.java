c4a2ab439c2f5efd96b3cceeefde395d4e9337bd
Leon Wetzel <leonwetzel@gmail.com>
2016-02-11 10:13:58 +0100

    Added exercise 9

---
+++ leertaak1Opgave9/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Calculates the hexadecimal value of a decimal integer.
+ * @author Leon Wetzel
+ *
+ */
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(Long.toString(3405691582L , 16));
+	}
+}
