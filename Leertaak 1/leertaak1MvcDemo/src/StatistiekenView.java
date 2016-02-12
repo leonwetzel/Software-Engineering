e38361de13f29f414704ed74ab9f00be4075796d
Leon Wetzel <leonwetzel@gmail.com>
2016-02-12 11:27:02 +0100

    Small changes concerning recent merge

--- Leertaak 1/leertaak1MvcDemo/src/StatistiekenView.java
+++ Leertaak 1/leertaak1MvcDemo/src/StatistiekenView.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.*;
-
-public class StatistiekenView extends JPanel implements ActionListener
-{
-    private JTextArea steenRoodVeld = new JTextArea();
-    DobbelsteenModel d;
-    
-
-    public StatistiekenView(DobbelsteenModel dobbelsteen)
-    {
-        this.setLayout(new FlowLayout());
-        this.add(steenRoodVeld);
-        this.d = dobbelsteen;
-        setField();
-    }
-    
-    
-
-    private void setField()
-    {
-    	String stats = new String();
-    	HashMap<Integer, Integer> theNumbers = d.getStats();
-    	for(int i = 1; i < 7; i++)
-    	{
-    		if(theNumbers.containsKey(i))
-    		{
-    			stats= stats + Integer.toString(i) + "-" + theNumbers.get(i);
-    		}
-    		else
-    		{
-    			stats= stats + Integer.toString(i) + "-" + "0";
-        		
-    		}
-    		stats = stats + "\n";
-    	}
-    	steenRoodVeld.setText(stats);
-    }
-
-    
-    public void actionPerformed( ActionEvent e )
-	{
-    	setField();
-	}
-
-    public Dimension getPreferredSize()
-    {
-        return new Dimension(50,50);
-    }
-    
-   
-=======
 import javax.swing.*;
 
 import java.awt.*;
@@ -94,7 +38,6 @@ public class StatistiekenView extends JPanel implements ActionListener
     	steenRoodVeld.setText(stats);
     }
 
-    
     public void actionPerformed( ActionEvent e )
 	{
     	setField();
@@ -104,7 +47,4 @@ public class StatistiekenView extends JPanel implements ActionListener
     {
         return new Dimension(50,50);
     }
-    
-   
->>>>>>> 46ce5bc671653fdf1bbb8e3bcc2ee57c879ab833
 }
