ed82122358443820401094b02cb42d65793bd8c0
Leon Wetzel <leonwetzel@gmail.com>
2016-02-17 20:18:40 +0100

    Altered main() in SnelheidOefening to display time in milliseconds

--- Leertaak 2/leertaak2GetalRij/SnelheidOefening.java
+++ Leertaak 2/leertaak2GetalRij/SnelheidOefening.java
@@ -6,6 +6,7 @@ public class SnelheidOefening {
 	 */
 	public static void main( String[] args){
 		GetalRij gr = new GetalRij( 100000, 200000);
+        System.out.println(tijd());
 	}
 
 	// Hulpmethode voor tijdsbepaling
