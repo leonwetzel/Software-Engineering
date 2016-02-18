b1538e4f81a2c679584d094e1ab8cb9ddfd5c22d
Leon Wetzel <leonwetzel@gmail.com>
2016-02-18 13:22:29 +0100

    Added skeleton class Traverser

---
+++ Leertaak 2/leertaak2Classifier/src/classifier/Traverser.java
@@ -0,0 +1,32 @@
+package classifier;
+
+/**
+ * Created by Leon Wetzel
+ * Date of creation 18-2-2016, 13:21
+ * |
+ * Authors: Leon Wetzel
+ * |
+ * Version: 1.0
+ * Package: classifier
+ * Class:
+ * Description:
+ * |
+ * |
+ * Changelog:
+ * 1.0:
+ */
+public class Traverser {
+    private Node root;
+
+    public Traverser() {
+
+    }
+
+    public void inorderTraversing() {
+
+    }
+
+    public void preoderTraversing() {
+
+    }
+}
