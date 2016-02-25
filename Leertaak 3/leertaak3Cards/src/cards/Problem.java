3b1584b5d97109106eb04cfe162d9ad384e9676e
metaladdict6 <r.r.c.ziengs@st.hanze.nl>
2016-02-25 13:59:14 +0100

    small change

--- Leertaak 3/leertaak3Cards/src/cards/Problem.java
+++ Leertaak 3/leertaak3Cards/src/cards/Problem.java
@@ -3,35 +3,42 @@ import java.util.Scanner;
 
 public class Problem
 {
-private Candidates candidates = new Candidates();
-private Solution   solution   = new Solution();
-private Scanner    reader = new Scanner(System.in);
-    
+    private Candidates candidates   = new Candidates();
+    private Solution   solution     = new Solution();
+    private Scanner    reader       = new Scanner(System.in);
+
     public void solve()
-{
-     //System.out.println(candidates);
-     //System.out.println(solution);
-     //reader.nextLine();
-     int index=0;
-     while (index<candidates.size())
-     {
-         if (solution.fits(candidates.get(index)))
-         {
-             solution.record(candidates.remove(index)); //move candidate to solution
-             if (solution.complete())
-             {
-                 solution.show();
-             }
-             else
-             {
-                 solve();
-             }
-             candidates.add(index, solution.eraseRecording()); //move candidate to candidates
-  
-           }
-           index++;
+    {
+        //System.out.println(candidates);
+        //System.out.println(solution);
+        //reader.nextLine();
+        int index=0;
+        while (index<candidates.size())
+        {
+            if (solution.fits(candidates.get(index)))
+            {
+                solution.record(candidates.remove(index)); //move candidate to solution
+                if (solution.complete())
+                {
+                    solution.show();
+                    break;
+                }
+                else
+                {
+                    solve();
+                }
+                candidates.add(index, solution.eraseRecording()); //move candidate to candidates
+
+            }
+            index++;
+            if(index == candidates.size())
+            {
+                index = 0;
+            }
+
+
         }
-}
+    }
 
 }
         
