6281ea0df6d13e1e870ae0dd533c2adbef7b0dde
Leon Wetzel <leonwetzel@gmail.com>
2016-03-02 20:57:19 +0100

    Added classes and code for Solvers

---
+++ Leertaak 3/leertaak3DynamicProgramming/src/dynProg/BottomUpSolver.java
@@ -0,0 +1,23 @@
+package dynProg;
+
+/**
+ * Created by Leon Wetzel
+ * Date of creation 2-3-2016, 20:54
+ * |
+ * Authors: Leon Wetzel
+ * |
+ * Version: 1.0
+ * Package: dynProg
+ * Class:
+ * Description:
+ * |
+ * |
+ * Changelog:
+ * 1.0:
+ */
+public class BottomUpSolver implements Solver {
+    @Override
+    public boolean solve(int[] numbers, int sum) {
+        return false;
+    }
+}
