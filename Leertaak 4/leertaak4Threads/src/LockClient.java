4f79c804e147a18f52657d6db9f260265cc617af
Leon Wetzel <leonwetzel@gmail.com>
2016-03-10 16:42:26 +0100

    hoi

--- Leertaak 4/leertaak4Threads/src/LockClient.java
+++ Leertaak 4/leertaak4Threads/src/LockClient.java
@@ -1,9 +1,14 @@
 /**
- * Class which manages threads.
+ * Class which manages threads by using a lock.
  * Created by Leon on 10-3-2016.
  */
-public class LockClient {
+public class LockClient implements Client {
 	public static void main(String[] args) {
+		new LockClient().setup();
+	}
+
+	@Override
+	public void setup() {
 		// Create threads by assigning runnables
 		Thread thread1 = new Thread(new LockThread());
 		Thread thread2 = new Thread(new LockThread());
--- Leertaak 4/leertaak4Threads/src/LockThread.java
+++ Leertaak 4/leertaak4Threads/src/LockThread.java
@@ -3,14 +3,10 @@
  * Created by Leon on 10-3-2016.
  */
 public class LockThread implements Runnable {
-	private final static int LIMIT = 4;
-
 	private static int amount = 0;
 	private final static Object lock = new Object();
 
-	public LockThread() {
-
-	}
+	public LockThread() {}
 
 	/**
 	 * Prints the amount twice in the command line.
