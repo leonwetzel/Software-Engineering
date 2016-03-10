ea642af2c408f883392afd6495385137166546d3
Leon Wetzel <leonwetzel@gmail.com>
2016-03-10 11:50:50 +0100

    added classes for threading exercises

---
+++ Leertaak 4/leertaak4Threads/src/LockThread.java
@@ -0,0 +1,13 @@
+/**
+ * Client for controlling threads using locks.
+ * Created by Leon on 10-3-2016.
+ */
+public class LockThread implements Runnable {
+	public LockThread() {}
+
+	@Override
+	public void run() {
+		// iets
+	}
+}
+
