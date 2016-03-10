9f309a5c55fef046065b5a266861d9e4448f59a3
Leon Wetzel <leonwetzel@gmail.com>
2016-03-10 13:37:01 +0100

    Opdracht 1 + Brugali

--- Leertaak 4/leertaak4Threads/src/LockClient.java
+++ Leertaak 4/leertaak4Threads/src/LockClient.java
@@ -1,10 +1,19 @@
 /**
- * Client for controlling threads using locks.
+ * Class which manages threads.
  * Created by Leon on 10-3-2016.
  */
 public class LockClient {
 	public static void main(String[] args) {
-		// doe iets
-	}
+		// Create threads by assigning runnables
+		Thread thread1 = new Thread(new LockThread());
+		Thread thread2 = new Thread(new LockThread());
+		Thread thread3 = new Thread(new LockThread());
+		Thread thread4 = new Thread(new LockThread());
 
+		// Start threads
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread4.start();
+	}
 }
